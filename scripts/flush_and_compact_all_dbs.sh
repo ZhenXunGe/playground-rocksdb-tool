ac5047794a21e0eb34c8733f4f50c25b5a93f6df
qozymandias <odowning19@gmail.com>
2025-05-01 14:03:17 +1000

    fix

--- scripts/flush_and_compact_all_dbs.sh
+++ scripts/flush_and_compact_all_dbs.sh
@@ -4,4 +4,4 @@ workspace=$(realpath "$1")
 
 md5_list=$(find "$workspace"/images/*/*/* -type d -print | xargs -n1 basename | paste -sd ' ')
 
-cargo run --release flush-and-compact --workspace "$workspace" --md5-list "$md5_list"
+cargo run --release flush-and-compact --workspace "$workspace" --md5-list $md5_list
